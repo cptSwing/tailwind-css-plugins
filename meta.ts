0e897cadfeeecc66b1eac1c2e3959ac09f7a6f3c
Jens <jens@jbrandenburg.de>
2024-11-23 11:05:33 +0100

    Update meta.ts

--- meta.ts
+++ meta.ts
@@ -1,4 +1,4 @@
 import tailwindClipInset from './twClipInset.ts';
 import tailwindMaskEdges from './twMaskEdges.ts';
 
-export default {tailwindClipInset, tailwindMaskEdges};
+export {tailwindClipInset, tailwindMaskEdges};
